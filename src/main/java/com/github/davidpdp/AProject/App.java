5295f4a8a3c357d2cecdff039a5a0a4ce72178a9
DavidPDP <ballesto_@hotmail.com>
2019-09-25 08:24:41 -0500

    baseline

---
+++ src/main/java/com/github/davidpdp/AProject/App.java
@@ -0,0 +1,13 @@
+package com.github.davidpdp.AProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
